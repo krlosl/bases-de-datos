2470d0978b1b1a0f92534c09a82ca334feae6d9d
krlosl <caarlosluzz@gmail.com>
2024-05-20 15:42:12 +0200

    Add files via upload

---
+++ ddbb/HAMMERSTRENGTH_entrenamientos.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: HAMMERSTRENGTH
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entrenamientos`
+--
+
+DROP TABLE IF EXISTS `entrenamientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entrenamientos` (
+  `idEntrenamiento` int NOT NULL,
+  `idUsuario` int NOT NULL,
+  `diasEntrenamiento` varchar(45) DEFAULT NULL,
+  `LUNES` varchar(999) DEFAULT NULL,
+  `MARTES` varchar(999) DEFAULT NULL,
+  `MIERCOLES` varchar(999) DEFAULT NULL,
+  `JUEVES` varchar(999) DEFAULT NULL,
+  `VIERNES` varchar(999) DEFAULT NULL,
+  `SABADO` varchar(999) DEFAULT NULL,
+  `DOMINGO` varchar(999) DEFAULT NULL,
+  PRIMARY KEY (`idEntrenamiento`),
+  UNIQUE KEY `idEntrenamiento_UNIQUE` (`idEntrenamiento`),
+  UNIQUE KEY `idUsuario_UNIQUE` (`idUsuario`),
+  CONSTRAINT `fk_idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entrenamientos`
+--
+
+LOCK TABLES `entrenamientos` WRITE;
+/*!40000 ALTER TABLE `entrenamientos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entrenamientos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-20 15:25:04
